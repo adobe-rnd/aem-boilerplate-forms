ac4218955b8347326751bd84bfecbed89795274a
Ravisanker E <rae@ravisankers-mbp.macromedia.com>
2024-07-29 17:01:37 +0530

    fix

--- blocks/form/components/modal.js
+++ blocks/form/components/modal.js
@@ -3,12 +3,12 @@ import { subscribe } from '../rules/index.js';
 
 export default async function decorate(panel) {
   const modal = await createModal(panel);
-  subscribe(panel, (fieldDiv, fieldModel, formModel) => {
-    if (formModel?.getElement(fieldDiv?.dataset?.id).visible === true) {
+  subscribe(panel, (fieldDiv, fieldModel) => {
+    const { visible } = fieldModel;
+    if (visible === true) {
       modal.showModal();
     }
   }, (fieldDiv, formModel) => {
-    console.log('updateFormModelCallback invoked');
     if (formModel) {
       formModel.getElement(fieldDiv.dataset.id).visible = false;
     }
