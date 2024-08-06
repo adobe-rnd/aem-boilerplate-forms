a58866dda3bd06471b36964361d94a7e67f84316
Ravisanker E <rae@Ravisankers-MacBook-Pro.local>
2024-08-06 14:52:56 +0530

    Added support for modal

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -357,8 +357,7 @@ export async function generateFormRendition(panel, container, getItems = (p) => 
       colSpanDecorator(field, element);
       const decorator = await componentDecorater(field);
       if (field?.fieldType === 'panel') {
-        await generateFormRendition(field, element, getItems);
-        return element;
+        return generateFormRendition(field, element, getItems);
       }
       if (typeof decorator === 'function') {
         return decorator(element, field, container);
