d49177cd11401b62204d3b2124bdd341732e8a14
Pranay Tippa <ptippa@adobe.com>
2024-08-09 16:48:09 +0530

    debugging

--- test/e2e/global-setup.js
+++ test/e2e/global-setup.js
@@ -18,31 +18,38 @@ const selectors = {
 };
 
 async function globalSetup() {
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext();
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext({ recordVideo: { dir: './videos/' } });
   const page = await context.newPage();
-  await page.goto(baseUrl, { waitUntil: 'networkidle' });
-  await page.locator(selectors.signInWithAdobe).click();
-  await expect(page.locator(selectors.createAnAccount)).toBeVisible();
-  await page.locator(selectors.googleSignIn).click();
-  await page.waitForLoadState('networkidle');
-  await expect(page.locator(selectors.headingText)).toBeVisible({ timeout: 20000 });
-  expect(await page.locator(selectors.headingText).innerText()).toBe('Sign in');
-  await page.locator(selectors.emailInput).fill(emailId);
-  await page.locator(selectors.emailInput).blur();
-  await page.locator(selectors.nextButton).click();
-  await expect(page.locator(selectors.forgotPassword)).toBeVisible({ timeout: 20000 });
-  expect(await page.locator('div[data-email][data-profile-identifier]').innerText()).toBe(emailId);
-  await page.locator(selectors.passwordInput).fill(password);
-  await page.locator(selectors.passwordInput).blur();
-  await page.locator(selectors.nextButton).click();
-  await page.waitForURL('https://author-p133911-e1313554.adobeaemcloud.com/ui#/aem/aem/start.html', { timeout: 30000 });
-  await page.waitForLoadState('load');
-  await page.waitForURL('https://author-p133911-e1313554.adobeaemcloud.com/ui#/aem/aem/start.html?appId=aemshell');
-  const frame = page.frameLocator(selectors.iFrame);
-  await expect(frame.getByLabel('Navigation')).toBeVisible();
-  await page.context().storageState({ path: filePath });
-  await browser.close();
+  try {
+    await page.goto(baseUrl, { waitUntil: 'networkidle' });
+    await page.locator(selectors.signInWithAdobe).click();
+    await expect(page.locator(selectors.createAnAccount)).toBeVisible();
+    await page.locator(selectors.googleSignIn).click();
+    await page.waitForLoadState('networkidle');
+    await expect(page.locator(selectors.headingText)).toBeVisible({ timeout: 20000 });
+    expect(await page.locator(selectors.headingText).innerText()).toBe('Sign in');
+    await page.locator(selectors.emailInput).fill(emailId);
+    await page.locator(selectors.emailInput).blur();
+    await page.locator(selectors.nextButton).click();
+    await expect(page.locator(selectors.forgotPassword)).toBeVisible({ timeout: 20000 });
+    expect(await page.locator('div[data-email][data-profile-identifier]').innerText()).toBe(emailId);
+    await page.locator(selectors.passwordInput).fill(password);
+    await page.locator(selectors.passwordInput).blur();
+    await page.locator(selectors.nextButton).click();
+    await page.waitForURL('https://author-p133911-e1313554.adobeaemcloud.com/ui#/aem/aem/start.html', { timeout: 30000 });
+    await page.waitForLoadState('load');
+    await page.waitForURL('https://author-p133911-e1313554.adobeaemcloud.com/ui#/aem/aem/start.html?appId=aemshell');
+    const frame = page.frameLocator(selectors.iFrame);
+    await expect(frame.getByLabel('Navigation')).toBeVisible();
+    await page.context().storageState({ path: filePath });
+  } catch (error) {
+    await page.screenshot({ path: './error-screenshot.png' });
+    throw error;
+  } finally {
+    await context.close();
+    await browser.close();
+  }
 }
 
 export default globalSetup;
