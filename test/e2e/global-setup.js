8da1ea179a99d6221d099dd05a3ed6e8fa7ef0aa
Pranay Tippa <ptippa@adobe.com>
2024-08-09 15:59:30 +0530

    debugging

--- test/e2e/global-setup.js
+++ test/e2e/global-setup.js
@@ -1,37 +1,48 @@
 import { chromium, expect } from '@playwright/test';
 
-const userName = process.env.AEM_username;
-const password = process.env.AEM_password;
-
 const filePath = './LoginAuth.json';
 const baseUrl = 'https://author-p133911-e1313554.adobeaemcloud.com/aem/start.html';
-const usernameInput = 'input[name="username"]';
-const passwordInput = 'input[name="password"]';
-const iFrame = 'iframe[id*="exc-app-sandbox"]';
-const continueButton = '[class="spectrum-Button-label"]';
-const emailValidation = '.Profile-Email';
-const error = '[class="description-big"] span';
-const createAnAccount = 'a[class="spectrum-Link EmailPage__create-account-link"]';
+const emailId = 'ptippa.test@gmail.com';
+const password = 'bqD_BsY2+m)32pV';
+
+const selectors = {
+  iFrame: 'iframe[id*="exc-app-sandbox"]',
+  createAnAccount: 'a[class="spectrum-Link EmailPage__create-account-link"]',
+  signInWithAdobe: 'text=Sign in with Adobe',
+  googleSignIn: 'section[data-social-buttons-container="regular"] a[data-provider="Google"]',
+  headingText: 'h1#headingText span',
+  emailInput: 'input[type="email"]',
+  nextButton: 'text=Next',
+  forgotPassword: 'text=Forgot password?',
+  passwordInput: 'input[type="password"][aria-label="Enter your password"]',
+};
 
 async function globalSetup() {
-  // eslint-disable-next-line no-console
-  const browser = await chromium.launch({ headless: true });
+  const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto(baseUrl, { waitUntil: 'networkidle' });
-  await page.getByText('Sign in with Adobe').click();
-  await expect(page.locator(createAnAccount)).toBeVisible();
-  await page.locator(usernameInput).fill(userName);
-  await page.locator(continueButton).click();
-
-  await console.log(`error ${await page.locator(error).first().innerText()}`);
-  expect(await page.locator(error).first().innerText()).toBe('ptippa+test@adobetest.com');
-  expect(await page.locator(emailValidation).innerText()).toBe(userName);
-  await page.locator(passwordInput).fill(password);
-  await page.getByLabel('Continue').click();
-  const frame = page.frameLocator(iFrame);
-  await expect(frame.getByLabel('Navigation')).toBeVisible({ timeout: 20000 });
+  await page.locator(selectors.signInWithAdobe).click();
+  await expect(page.locator(selectors.createAnAccount)).toBeVisible();
+  await page.locator(selectors.googleSignIn).click();
+  await page.waitForLoadState('networkidle');
+  await expect(page.locator(selectors.headingText)).toBeVisible({ timeout: 20000 });
+  expect(await page.locator(selectors.headingText).innerText()).toBe('Sign in');
+  await page.locator(selectors.emailInput).fill(emailId);
+  await page.locator(selectors.emailInput).blur();
+  await page.locator(selectors.nextButton).click();
+  await expect(page.locator(selectors.forgotPassword)).toBeVisible({ timeout: 20000 });
+  expect(await page.locator('div[data-email][data-profile-identifier]').innerText()).toBe(emailId);
+  await page.locator(selectors.passwordInput).fill(password);
+  await page.locator(selectors.passwordInput).blur();
+  await page.locator(selectors.nextButton).click();
+  await page.waitForURL('https://author-p133911-e1313554.adobeaemcloud.com/ui#/aem/aem/start.html', { timeout: 30000 });
+  await page.waitForLoadState('load');
+  await page.waitForURL('https://author-p133911-e1313554.adobeaemcloud.com/ui#/aem/aem/start.html?appId=aemshell');
+  const frame = page.frameLocator(selectors.iFrame);
+  await expect(frame.getByLabel('Navigation')).toBeVisible();
   await page.context().storageState({ path: filePath });
   await browser.close();
 }
+
 export default globalSetup;
