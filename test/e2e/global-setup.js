b38cd934f627c99931a22a396e14f40e4b84ef52
Pranay Tippa <ptippa@adobe.com>
2024-08-13 13:13:24 +0530

    microsoft-login

--- test/e2e/global-setup.js
+++ test/e2e/global-setup.js
@@ -2,40 +2,41 @@ import { chromium, expect } from '@playwright/test';
 
 const filePath = './LoginAuth.json';
 const baseUrl = 'https://author-p133911-e1313554.adobeaemcloud.com/aem/start.html';
-const emailId = 'ptippa.test@gmail.com';
+const emailId = 'ptippa.test@outlook.com';
 const password = 'bqD_BsY2+m)32pV';
 
 const selectors = {
   iFrame: 'iframe[id*="exc-app-sandbox"]',
   createAnAccount: 'a[class="spectrum-Link EmailPage__create-account-link"]',
   signInWithAdobe: 'text=Sign in with Adobe',
-  googleSignIn: 'section[data-social-buttons-container="regular"] a[data-provider="Google"]',
-  headingText: 'h1#headingText span',
   emailInput: 'input[type="email"]',
-  nextButton: 'text=Next',
-  forgotPassword: 'text=Forgot password?',
-  passwordInput: 'input[type="password"][aria-label="Enter your password"]',
+  passwordInput: 'input[type="password"]',
+  userDisplayName: 'div#userDisplayName',
+  firstExtButtonItem: 'div[class*="ext-button-item"]:first-child',
 };
 
 async function globalSetup() {
-  const browser = await chromium.launch({ headless: true });
+  const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext({ recordVideo: { dir: './videos/' } });
   const page = await context.newPage();
   try {
     await page.goto(baseUrl, { waitUntil: 'networkidle' });
     await page.locator(selectors.signInWithAdobe).click();
     await expect(page.locator(selectors.createAnAccount)).toBeVisible();
-    await page.locator(selectors.googleSignIn).click();
+    await page.getByRole('link', { name: 'View more' }).click();
+    await expect(page.getByRole('button', { name: 'Continue with Microsoft' })).toBeVisible();
+    await page.getByRole('button', { name: 'Continue with Microsoft' }).click();
     await page.waitForLoadState('networkidle');
-    await expect(page.getByText('Forgot email?')).toBeVisible({ timeout: 15000 });
+    await expect(page.locator(selectors.emailInput)).toBeVisible();
     await page.locator(selectors.emailInput).fill(emailId);
     await page.locator(selectors.emailInput).blur();
-    await page.locator(selectors.nextButton).click();
-    await expect(page.locator(selectors.forgotPassword)).toBeVisible({ timeout: 20000 });
-    expect(await page.locator('div[data-email][data-profile-identifier]').innerText()).toBe(emailId);
-    await page.getByLabel('Enter your password').fill(password);
-    await page.getByLabel('Enter your password').blur();
-    await page.locator(selectors.nextButton).click();
+    await page.getByRole('button', { name: 'Next' }).click();
+    expect(await page.locator(selectors.userDisplayName).innerText()).toBe(emailId);
+    await page.locator(selectors.passwordInput).fill(password);
+    await page.locator(selectors.passwordInput).blur();
+    await page.getByRole('button', { name: 'Sign in' }).click();
+    await expect(page.getByText('Stay signed in?')).toBeVisible();
+    await page.locator(selectors.firstExtButtonItem).first().click();
     await page.waitForURL('https://author-p133911-e1313554.adobeaemcloud.com/ui#/aem/aem/start.html', { timeout: 30000 });
     await page.waitForLoadState('load');
     await page.waitForURL('https://author-p133911-e1313554.adobeaemcloud.com/ui#/aem/aem/start.html?appId=aemshell');
