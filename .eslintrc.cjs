52c40e7c4ead968bea235b59d4d4fc6ec67ea5a8
Nitin Gupta <nitigupt@adobe.com>
2024-08-16 12:07:26 +0530

    chore(ue): window.alert if annotation errors out

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,7 @@ module.exports = {
   root: true,
   extends: [
     'airbnb-base',
-    'plugin:json/recommended',
-    'plugin:xwalk/recommended',
+    'plugin:json/recommended'
   ],
   env: {
     browser: true,
@@ -18,5 +17,6 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'import/no-extraneous-dependencies': ["error", {"devDependencies": true}]
   },
 };
