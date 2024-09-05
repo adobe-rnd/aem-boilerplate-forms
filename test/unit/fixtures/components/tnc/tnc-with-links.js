30447f4b60d3585ce37acad6bb84694e60b88e8b
Talmiz Ahmed <talmiza@adobe.com>
2024-09-05 11:19:14 +0530

    Fixing linting errors

--- test/unit/fixtures/components/tnc/tnc-with-links.js
+++ test/unit/fixtures/components/tnc/tnc-with-links.js
@@ -2,111 +2,111 @@
 export const fieldDef = {
   items: [
     {
-      id: "termsandconditions-a3e8065f4f",
-      fieldType: "panel",
-      name: "termsandconditions1725423902420",
+      id: 'termsandconditions-a3e8065f4f',
+      fieldType: 'panel',
+      name: 'termsandconditions1725423902420',
       visible: true,
       enabled: true,
       properties: {
         'fd:dor': {
           dorExclusion: false,
           dorExcludeTitle: false,
-          dorExcludeDescription: false
+          dorExcludeDescription: false,
         },
-        'fd:path': "/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions"
+        'fd:path': '/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions',
       },
-      gridClassNames: "aem-Grid aem-Grid--12 aem-Grid--default--12",
+      gridClassNames: 'aem-Grid aem-Grid--12 aem-Grid--default--12',
       columnCount: 12,
       columnClassNames: {
-        approvalcheckbox: "aem-GridColumn aem-GridColumn--default--12",
-        link: "aem-GridColumn aem-GridColumn--default--12",
-        text: "aem-GridColumn aem-GridColumn--default--12"
+        approvalcheckbox: 'aem-GridColumn aem-GridColumn--default--12',
+        link: 'aem-GridColumn aem-GridColumn--default--12',
+        text: 'aem-GridColumn aem-GridColumn--default--12',
       },
       label: {
-        value: "Terms and conditions"
+        value: 'Terms and conditions',
       },
       events: {
         'custom:setProperty': [
-          "$event.payload"
-        ]
+          '$event.payload',
+        ],
       },
       ':itemsOrder': [
-        "link",
-        "approvalcheckbox"
+        'link',
+        'approvalcheckbox',
       ],
       allowedComponents: {
         applicable: false,
-        components: []
+        components: [],
       },
       items: [
         {
-          id: "checkboxgroup-4bc71e8509",
-          fieldType: "checkbox-group",
-          name: "link1725423922865",
+          id: 'checkboxgroup-4bc71e8509',
+          fieldType: 'checkbox-group',
+          name: 'link1725423922865',
           visible: true,
-          type: "string[]",
+          type: 'string[]',
           enabled: true,
           enforceEnum: true,
           enumNames: [
-            "Terms and conditions of the service.",
-            "Privacy policy."
+            'Terms and conditions of the service.',
+            'Privacy policy.',
           ],
           label: {
-            value: ""
+            value: '',
           },
           events: {
             change: [
-              "if(length($field.$value) == length($field.$enum), dispatchEvent($parent.approvalcheckbox, 'custom:setProperty', {enabled : true()}), {})"
+              "if(length($field.$value) == length($field.$enum), dispatchEvent($parent.approvalcheckbox, 'custom:setProperty', {enabled : true()}), {})",
             ],
             'custom:setProperty': [
-              "$event.payload"
-            ]
+              '$event.payload',
+            ],
           },
           properties: {
             'afs:layout': {
-              orientation: "horizontal"
+              orientation: 'horizontal',
             },
             'fd:dor': {
-              dorExclusion: false
+              dorExclusion: false,
             },
-            'fd:path': "/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions/link",
+            'fd:path': '/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions/link',
             'fd:rules': {
-              validationStatus: "valid"
-            }
+              validationStatus: 'valid',
+            },
           },
           enum: [
-            "dummy-link-1",
-            "dummy-link-2"
+            'dummy-link-1',
+            'dummy-link-2',
           ],
-          ':type': "toggleable-link"
+          ':type': 'toggleable-link',
         },
         {
-          id: "checkbox-de8f4d881d",
-          fieldType: "checkbox",
-          name: "approvalcheckbox",
+          id: 'checkbox-de8f4d881d',
+          fieldType: 'checkbox',
+          name: 'approvalcheckbox',
           visible: true,
-          type: "string",
+          type: 'string',
           required: true,
           enabled: true,
           enforceEnum: true,
           label: {
-            value: "I agree to the terms & conditions."
+            value: 'I agree to the terms & conditions.',
           },
           events: {
             'custom:setProperty': [
-              "$event.payload"
-            ]
+              '$event.payload',
+            ],
           },
           properties: {
             'fd:dor': {
-              dorExclusion: false
+              dorExclusion: false,
             },
-            'fd:path': "/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions/approvalcheckbox"
+            'fd:path': '/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions/approvalcheckbox',
           },
-          ':type': "core/fd/components/form/checkbox/v1/checkbox"
-        }
+          ':type': 'core/fd/components/form/checkbox/v1/checkbox',
+        },
       ],
-      ':type': "tnc"
-    }
+      ':type': 'tnc',
+    },
   ],
 };
--- test/unit/fixtures/components/tnc/tnc.js
+++ test/unit/fixtures/components/tnc/tnc.js
@@ -9,90 +9,90 @@ global.IntersectionObserver = sinon.stub().returns({
 export const fieldDef = {
   items: [
     {
-      id: "termsandconditions-a3e8065f4f",
-      fieldType: "panel",
-      name: "termsandconditions1725421129697",
+      id: 'termsandconditions-a3e8065f4f',
+      fieldType: 'panel',
+      name: 'termsandconditions1725421129697',
       visible: true,
       enabled: true,
       properties: {
         'fd:dor': {
           dorExclusion: false,
           dorExcludeTitle: false,
-          dorExcludeDescription: false
+          dorExcludeDescription: false,
         },
-        'fd:path': "/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions"
+        'fd:path': '/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions',
       },
-      gridClassNames: "aem-Grid aem-Grid--12 aem-Grid--default--12",
+      gridClassNames: 'aem-Grid aem-Grid--12 aem-Grid--default--12',
       columnCount: 12,
       columnClassNames: {
-        approvalcheckbox: "aem-GridColumn aem-GridColumn--default--12",
-        link: "aem-GridColumn aem-GridColumn--default--12",
-        text: "aem-GridColumn aem-GridColumn--default--12"
+        approvalcheckbox: 'aem-GridColumn aem-GridColumn--default--12',
+        link: 'aem-GridColumn aem-GridColumn--default--12',
+        text: 'aem-GridColumn aem-GridColumn--default--12',
       },
       label: {
-        value: "Terms and conditions"
+        value: 'Terms and conditions',
       },
       events: {
         'custom:setProperty': [
-          "$event.payload"
-        ]
+          '$event.payload',
+        ],
       },
       ':itemsOrder': [
-        "text",
-        "approvalcheckbox"
+        'text',
+        'approvalcheckbox',
       ],
       allowedComponents: {
         applicable: false,
-        components: []
+        components: [],
       },
       items: [
         {
-          id: "text-e9e5c0bb43",
+          id: 'text-e9e5c0bb43',
           dataRef: null,
-          fieldType: "plain-text",
-          name: "text1725421129728",
-          value: "Text related to the terms and conditions come here.",
+          fieldType: 'plain-text',
+          name: 'text1725421129728',
+          value: 'Text related to the terms and conditions come here.',
           richText: true,
           events: {
             'custom:setProperty': [
-              "$event.payload"
-            ]
+              '$event.payload',
+            ],
           },
           properties: {
             'fd:dor': {
-              dorExclusion: false
+              dorExclusion: false,
             },
-            'fd:path': "/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions/text"
+            'fd:path': '/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions/text',
           },
-          ':type': "core/fd/components/form/text/v1/text"
+          ':type': 'core/fd/components/form/text/v1/text',
         },
         {
-          id: "checkbox-de8f4d881d",
-          fieldType: "checkbox",
-          name: "approvalcheckbox",
+          id: 'checkbox-de8f4d881d',
+          fieldType: 'checkbox',
+          name: 'approvalcheckbox',
           visible: true,
-          type: "string",
+          type: 'string',
           required: true,
           enabled: true,
           enforceEnum: true,
           label: {
-            value: "I agree to the terms & conditions."
+            value: 'I agree to the terms & conditions.',
           },
           events: {
             'custom:setProperty': [
-              "$event.payload"
-            ]
+              '$event.payload',
+            ],
           },
           properties: {
             'fd:dor': {
-              dorExclusion: false
+              dorExclusion: false,
             },
-            'fd:path': "/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions/approvalcheckbox"
+            'fd:path': '/content/talmiz-test-site/index/jcr:content/root/section_2/form/termsandconditions/approvalcheckbox',
           },
-          ':type': "core/fd/components/form/checkbox/v1/checkbox"
-        }
+          ':type': 'core/fd/components/form/checkbox/v1/checkbox',
+        },
       ],
-      ':type': "tnc"
-    }
+      ':type': 'tnc',
+    },
   ],
 };
