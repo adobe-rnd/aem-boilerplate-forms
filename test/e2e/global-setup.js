9c84dadd4700b8e80f674c2720341920d68ec957
ujjgupta <ujjgupta@adobe.com>
2024-11-11 14:11:14 +0530

    Fixing lint

--- test/e2e/global-setup.js
+++ test/e2e/global-setup.js
@@ -2,10 +2,8 @@ import { chromium, expect } from '@playwright/test';
 
 const filePath = './LoginAuth.json';
 const baseUrl = 'https://author-p133911-e1313554.adobeaemcloud.com/aem/start.html';
-// const emailId = process.env.AEM_userName;
-// const password = process.env.AEM_password;
-const emailId = 'ptippa.test@outlook.com';
-const password = 'bqD_BsY2+m)32pV';
+const emailId = process.env.AEM_userName;
+const password = process.env.AEM_password;
 
 const selectors = {
   iFrame: 'iframe[id*="exc-app-sandbox"]',
