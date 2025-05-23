7011e8a503d878ff3c8041af9d0262ece6e9e2f4
Pranay Tippa <ptippa@adobe.com>
2025-05-23 12:34:48 +0530

    test: validation of multisheet Doc-Based Form and Embed component

--- test/e2e/doc-based/multiSheetValidation.spec.js
+++ test/e2e/doc-based/multiSheetValidation.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import {openPage} from "../../utils.js";
+import {openPage} from "../utils.js";
 
 let requestPayload = null;
 const expectedPayload = {
