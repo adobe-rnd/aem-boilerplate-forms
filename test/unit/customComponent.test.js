fdbe0fd49219d32fb37d7084fa23b52f628ebdcd
Talmiz Ahmed <talmiz.tech@gmail.com>
2025-08-07 12:38:41 +0530

    fixing tests

--- test/unit/customComponent.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env mocha */
-import { executeTestInFolder, testBasicMarkup } from './testUtils.js';
-
-// Custom component test with custom components array
-function testCustomComponent(filePath, bUrlMode = false) {
-  testBasicMarkup(filePath, bUrlMode, ['range'], '../..');
-}
-
-// executeTestInFolder('./test/unit/fixtures/custom-components/', testCustomComponent);
