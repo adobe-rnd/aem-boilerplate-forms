ee0feaf7ec0a84869cb8d363c9dfbf6ca6ae308f
Ravisanker E <rae@ravisankers-mbp.corp.adobe.com>
2025-08-07 12:38:59 +0530

    minor comment refactor

--- test/e2e/x-walk/variables.spec.js
+++ test/e2e/x-walk/variables.spec.js
@@ -6,7 +6,7 @@ test.describe('form variables test suite', () => {
 const url = '/content/aem-boilerplate-forms-xwalk-collaterals/variables?accountCode=A1234&utm_campaign=test_campaign';
 test('check query params and browser details are getting set in properties', async ({ page }) => {
     await openPage(page, url);
-    // on form init an api is invoked and its reponse is set as the value of text input field
+    // the value of the textinput is set via rule editor to the query params passed
     const accountCode = await page.getByLabel('Account Code');
     const utmCampaign = await page.getByLabel('UTM Campaign');
     const browserLanguage = await page.getByLabel('Browser Language');
