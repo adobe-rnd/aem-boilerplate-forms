f1632298042c742cc5418361495bb4c916a0463d
Ravisanker E <rae@Ravisankers-MacBook-Pro.local>
2025-09-09 20:44:54 +0530

    added dynamic text

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -1,6 +1,6 @@
 import { loadCSS } from '../../scripts/aem.js';
 
-let customComponents = ['range', 'pan-input', 'others-checkbox'];
+let customComponents = ['range', 'pan-input', 'others-checkbox', 'dynamic-text'];
 const OOTBComponentDecorators = ['accordion', 'file', 'modal', 'password', 'rating', 'repeat', 'tnc', 'toggleable-link', 'wizard'];
 
 export function setCustomComponents(components) {
