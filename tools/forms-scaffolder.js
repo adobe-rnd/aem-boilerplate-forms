697127f21133a4fda350c395976258f294536439
Ravisanker E <rae@Ravisankers-MacBook-Pro.local>
2025-09-12 18:21:24 +0530

    minor: jsdoc update

--- tools/forms-scaffolder.js
+++ tools/forms-scaffolder.js
@@ -224,7 +224,7 @@ function createComponentFiles(componentName, baseComponent, targetDir) {
 
 /**
  * Decorates a custom form field component
- * @param {HTMLElement} fieldDiv - The DOM element containing the field wrapper. Refer to the documentation for its structure for each component.
+ * @param {HTMLElement} fieldDiv - The DOM element containing the field wrapper.
  * @param {Object} fieldJson - The form json object for the component.
  * @param {HTMLElement} parentElement - The parent element of the field.
  * @param {string} formId - The unique identifier of the form.
