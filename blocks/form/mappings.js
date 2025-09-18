af31d7e269f4108620cdc4398b1c8123525b773e
Talmiz Ahmed <talmiz.tech@gmail.com>
2025-09-18 12:05:43 +0530

    test

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -1,6 +1,6 @@
 import { loadCSS } from '../../scripts/aem.js';
 
-let customComponents = ['range'];
+let customComponents = ['composite-addr', 'range', 'reag', 'simple-custom-comp'];
 const OOTBComponentDecorators = ['accordion', 'file', 'modal', 'password', 'rating', 'repeat', 'tnc', 'toggleable-link', 'wizard'];
 
 export function setCustomComponents(components) {
