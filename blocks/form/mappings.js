a2699c7910abb92ecfd293f4ed324e13f8d3e3f3
Ravisanker E <rae@ravisankers-mbp.macromedia.com>
2025-09-22 10:12:55 +0530

    feat: add radio-card custom component

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -1,6 +1,6 @@
 import { loadCSS } from '../../scripts/aem.js';
 
-let customComponents = ['pan-input', 'range', 'sample'];
+let customComponents = ['pan-input', 'radio-card', 'range', 'sample'];
 const OOTBComponentDecorators = ['accordion', 'file', 'modal', 'password', 'rating', 'repeat', 'tnc', 'toggleable-link', 'wizard'];
 
 export function setCustomComponents(components) {
