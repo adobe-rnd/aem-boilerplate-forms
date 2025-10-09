023ba13d50715d64163115df3209f64b8f7bb15e
Pranay Tippa <ptippa@adobe.com>
2025-10-09 15:51:26 +0530

    adding video and screeshot to failure in AEM login

--- test/e2e/global-setup.js
+++ test/e2e/global-setup.js
@@ -7,30 +7,42 @@ const password = process.env.AEM_password;
 
 const selectors = {
   iFrame: 'iframe[id*="exc-app-sandbox"]',
-  continueButton : 'div[class*="EmailPage"] button[type="submit"]',
+  continueButton: 'div[class*="EmailPage"] button[type="submit"]',
   submitButton: 'div[class$="PasswordPage"] button[type="submit"]'
 };
 
 async function globalSetup() {
   const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext();
+  // Enable video recording here
+  const context = await browser.newContext({ recordVideo: { dir: './videos/' } });
   const page = await context.newPage();
-  await page.goto(baseUrl, { waitUntil: 'networkidle' });
-  await page.getByRole('button', { name: 'Sign in with Adobe' }).click();
-  await expect(page.getByText('Create an account').last()).toBeVisible();
-  await page.getByLabel('Email address').fill(emailId);
-  await page.locator(selectors.continueButton).click();
-  await expect(page.getByText('Enter your password')).toBeVisible();
-  await page.getByLabel('Password').first().fill(password);
-  await page.locator(selectors.submitButton).click();
 
-  const finalUrlPattern = '**/aem/aem/start.html?appId=aemshell';
-  await page.waitForURL(finalUrlPattern, { timeout: 45000 });
-  const frame = page.frameLocator(selectors.iFrame);
-  await expect(frame.getByLabel('Navigation')).toBeVisible();
-  await page.context().storageState({ path: filePath });
-  await context.close();
-  await browser.close();
+  try {
+    await page.goto(baseUrl, { waitUntil: 'networkidle' });
+    await page.getByRole('button', { name: 'Sign in with Adobe' }).click();
+    await expect(page.getByText('Create an account').last()).toBeVisible();
+    await page.getByLabel('Email address').fill(emailId);
+    await page.locator(selectors.continueButton).click();
+    await expect(page.getByText('Enter your password')).toBeVisible();
+    await page.getByLabel('Password').first().fill(password);
+    await page.locator(selectors.submitButton).click();
+
+    const finalUrlPattern = '**/aem/aem/start.html?appId=aemshell';
+    await page.waitForURL(finalUrlPattern, { timeout: 45000 });
+
+    const frame = page.frameLocator(selectors.iFrame);
+    await expect(frame.getByLabel('Navigation')).toBeVisible();
+
+    await page.context().storageState({ path: filePath });
+
+  } catch (error) {
+    // Screenshot on failure
+    await page.screenshot({ path: './error-screenshot.png' });
+    throw error;
+  } finally {
+    await context.close();  // this will save the video
+    await browser.close();
+  }
 }
 
 export default globalSetup;
