3f6b143bfb46653315ca8d7c4ed5a3fe14c176f8
Ravisanker E <rae@Ravisankers-MacBook-Pro.local>
2025-10-09 15:22:06 +0530

    Revert "feat: added countdown timer"
    This reverts commit 3bbb4e94e910d543cb923123f54e652ca3d19c0f.

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -1,6 +1,6 @@
 import { loadCSS } from '../../scripts/aem.js';
 
-let customComponents = ['countdowntimer', 'range', 'sample'];
+let customComponents = ['range'];
 const OOTBComponentDecorators = ['accordion', 'file', 'modal', 'password', 'rating', 'repeat', 'tnc', 'toggleable-link', 'wizard'];
 
 export function setCustomComponents(components) {
